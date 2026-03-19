636d14e8ec44745058433c8a3d2230075719ff29
Matheus Nobre <matheus1204h@gmail.com>
2026-03-18 22:08:47 -0300

    add header

--- cantinho/app/index.tsx
+++ cantinho/app/index.tsx
@@ -1,10 +1,27 @@
-import { Text, View, ScrollView, StyleSheet } from "react-native";
+import { Stack } from "expo-router";
+import { Text, View, ScrollView, StyleSheet, Pressable } from "react-native";
 import CardPostList from "./components/cardPostList/cardPostList";
 import SearchInput from "./components/searchInput/searchInput";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <ScrollView>
+
+      <Stack.Screen
+        options={{
+          title: "Cantinho do Professor",
+          headerStyle: {backgroundColor: '#473469'},
+          headerTintColor: '#fff',
+          headerTitleAlign: 'left',
+          headerRight: () => (
+            <Pressable style={{marginRight:15}}>
+              <Ionicons name="person-circle-outline" size={32} color='#ffffff' />
+            </Pressable>
+          )
+        }}
+      />
+
       <View style={styles.container}>
         <SearchInput />
         <CardPostList />
@@ -17,5 +34,6 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 40,
   }
 });
